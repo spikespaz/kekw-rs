a6258feb9e78a5d52b0c888f389ee805ddfa3386
Jacob Birkett <jacob@birkett.dev>
2024-04-05 01:05:33 -0700

    macros: ext: impl get_by_ident and filter_by_ident for AttributesExt

--- kekw_macros/src/ext.rs
+++ kekw_macros/src/ext.rs
@@ -24,6 +24,16 @@ pub(crate) trait AttributesExt {
         I: ?Sized,
         Ident: PartialEq<I>;
 
+    fn get_by_ident<I>(&self, ident: &I) -> Option<&Attribute>
+    where
+        I: ?Sized,
+        Ident: PartialEq<I>;
+
+    fn filter_by_ident<I>(&self, ident: &I) -> Vec<&Attribute>
+    where
+        I: ?Sized,
+        Ident: PartialEq<I>;
+
     fn pop_by_ident<I>(&mut self, ident: &I) -> Option<Attribute>
     where
         I: ?Sized,
@@ -37,6 +47,24 @@ impl AttributesExt for Vec<Attribute> {
         Ident: PartialEq<I>,
     {
         self.iter().position(|attr| attr.path().is_ident(ident))
+    }
+
+    fn get_by_ident<I>(&self, ident: &I) -> Option<&Attribute>
+    where
+        I: ?Sized,
+        Ident: PartialEq<I>,
+    {
+        self.iter().find(|attr| attr.path().is_ident(ident))
+    }
+
+    fn filter_by_ident<I>(&self, ident: &I) -> Vec<&Attribute>
+    where
+        I: ?Sized,
+        Ident: PartialEq<I>,
+    {
+        self.iter()
+            .filter(|attr| attr.path().is_ident(ident))
+            .collect()
     }
 
     fn pop_by_ident<I>(&mut self, ident: &I) -> Option<Attribute>
