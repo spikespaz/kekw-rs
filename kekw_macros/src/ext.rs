8a7ebdc5e426eabb20f898fe725a32a77159d660
Jacob Birkett <jacob@birkett.dev>
2024-04-05 00:58:36 -0700

    macros: ext: attributes find_by_ident

--- kekw_macros/src/ext.rs
+++ kekw_macros/src/ext.rs
@@ -19,6 +19,11 @@ pub(crate) use err;
 use syn::{Attribute, Ident};
 
 pub(crate) trait AttributesExt {
+    fn find_by_ident<I>(&self, ident: &I) -> Option<usize>
+    where
+        I: ?Sized,
+        Ident: PartialEq<I>;
+
     fn pop_by_ident<I>(&mut self, ident: &I) -> Option<Attribute>
     where
         I: ?Sized,
@@ -26,13 +31,19 @@ pub(crate) trait AttributesExt {
 }
 
 impl AttributesExt for Vec<Attribute> {
+    fn find_by_ident<I>(&self, ident: &I) -> Option<usize>
+    where
+        I: ?Sized,
+        Ident: PartialEq<I>,
+    {
+        self.iter().position(|attr| attr.path().is_ident(ident))
+    }
+
     fn pop_by_ident<I>(&mut self, ident: &I) -> Option<Attribute>
     where
         I: ?Sized,
         Ident: PartialEq<I>,
     {
-        self.iter()
-            .position(|attr| attr.path().is_ident(ident))
-            .map(|index| self.remove(index))
+        self.find_by_ident(ident).and_then(|i| Some(self.remove(i)))
     }
 }
