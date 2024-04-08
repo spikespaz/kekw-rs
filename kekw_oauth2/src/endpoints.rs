71be0439a16de82ea15b45dad8c41131050d15d3
Jacob Birkett <jacob@birkett.dev>
2024-04-07 23:14:26 -0700

    oauth2: endpoints: impl AuthCodeQuery to derive QueryParams

--- kekw_oauth2/src/endpoints.rs
+++ kekw_oauth2/src/endpoints.rs
@@ -1,54 +1,45 @@
 use std::fmt;
-use std::str::FromStr;
 
-use http_types::{Method, Request, Url};
-use kekw_types::url::QueryPairs;
-use percent_encoding::{percent_encode, CONTROLS};
+use kekw_macros::QueryParams;
 use serde::{Deserialize, Serialize};
 use typed_builder::TypedBuilder;
+use url::Url;
 
 use crate::types::Scopes;
 
 static AUTHORIZE_CODE_REQUEST_URL: &str = "https://id.twitch.tv/oauth2/authorize";
 
+fn unwrap_option<T>(opt: &Option<T>) -> &T {
+    opt.as_ref().unwrap()
+}
+
+fn percent_encode(source: impl ToString) -> String {
+    use percent_encoding::NON_ALPHANUMERIC;
+    percent_encoding::percent_encode(source.to_string().as_ref(), NON_ALPHANUMERIC).collect()
+}
+
 /// [Authorization code grant flow][1]
 ///
 /// [1]: https://dev.twitch.tv/docs/authentication/getting-tokens-oauth/#authorization-code-grant-flow
-#[derive(Serialize, TypedBuilder)]
-#[builder(build_method(into = QueryPairs<'static>))]
+#[derive(Serialize, Deserialize, TypedBuilder, QueryParams)]
 pub struct AuthCodeQuery {
     pub client_id: String,
     #[builder(setter(strip_bool))]
+    #[query_param(skip_if = |x: &bool| !x)]
     pub force_verify: bool,
-    pub redirect_uri: Url,
+    pub redirect_uri: String,
+    #[query_param(skip_if = Vec::is_empty, proxy = percent_encode)]
     pub scope: Scopes,
-    #[builder(setter(strip_option))]
+    #[builder(default, setter(strip_option))]
+    #[query_param(skip_if = Option::is_none, proxy = unwrap_option)]
     pub state: Option<String>,
 }
 
 impl From<AuthCodeQuery> for Url {
-    fn from(other: AuthCodeQuery) -> Url {
-        let mut url = Url::from_str(AUTHORIZE_CODE_REQUEST_URL).unwrap();
-        url.set_query(Some(
-            &serde_qs::to_string(&other).expect("failed to serialize query"),
-        ));
+    fn from(query: AuthCodeQuery) -> Url {
+        let mut url = Url::parse(AUTHORIZE_CODE_REQUEST_URL).unwrap();
+        url.set_query(Some(&query.to_string()));
         url
-    }
-}
-
-impl From<AuthCodeQuery> for QueryPairs<'static> {
-    fn from(other: AuthCodeQuery) -> Self {
-        Self::from_iter([
-            ("client_id", Some(other.client_id)),
-            ("force_verify", Some(other.force_verify.to_string())),
-            ("redirect_uri", Some(other.redirect_uri.to_string())),
-            ("response_type", Some("code".into())),
-            (
-                "scope",
-                Some(percent_encode(other.scope.to_string().as_bytes(), CONTROLS).to_string()),
-            ),
-            ("state", other.state),
-        ])
     }
 }
 
@@ -74,7 +65,22 @@ impl fmt::Display for AuthCodeDenied {
 
 impl std::error::Error for AuthCodeDenied {}
 
-// #[cfg(test)]
-// mod test {
-//     fn
-// }
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::types::{Scope, Scopes};
+
+    #[test]
+    fn test_display_query() {
+        let compare = "?client_id=oogabooga&redirect_uri=https://localhost:8083&scope=analytics%3Aread%3Aextensions%20channel%3Abot";
+        let data = AuthCodeQuery::builder()
+            .client_id("oogabooga".into())
+            .redirect_uri("https://localhost:8083".into())
+            .scope(Scopes::from_iter([
+                Scope::AnalyticsReadExtensions,
+                Scope::ChannelBot,
+            ]))
+            .build();
+        assert_eq!(compare, data.to_string());
+    }
+}
